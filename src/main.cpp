2e1b87f7b43b344931e7b4e6f64c18ac499cae40
NiniDev <linantfin@gmail.com>
2024-02-09 16:14:09 +0100

    Add World class and update main.cpp to use it

--- src/main.cpp
+++ src/main.cpp
@@ -3,55 +3,14 @@
 #include <SDL_ttf.h>
 #include <vector>
 
+#include "world.h"
+
 using std::cout;
 using std::endl;
 
 int main()
 {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << endl;
-    }
-
-    // create window
-    SDL_Window *window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
-
-    if (window == NULL)
-    {
-        cout << "Window could not be created! SDL_Error: " << SDL_GetError() << endl;
-    }
-
-    // create renderer
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    if (renderer == NULL)
-    {
-        cout << "Renderer could not be created! SDL_Error: " << SDL_GetError() << endl;
-    }
-
-    SDL_Event e;
-    bool quit = false;
-
-    while (!quit)
-    {
-        while (SDL_PollEvent(&e) != 0)
-        {
-            if (e.type == SDL_QUIT)
-            {
-                quit = true;
-            }
-        }
-
-        // clear screen
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-        SDL_RenderClear(renderer);
-
-        SDL_RenderPresent(renderer);
-    }
-
-    TTF_Quit();
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    World world;
+    world.startSimulation();
     return 0;
 }
---
+++ src/world.h
@@ -0,0 +1,29 @@
+#ifndef WORLD_H
+#define WORLD_H
+
+#include <SDL2/SDL.h>
+
+class World
+{
+public:
+    World();
+    ~World();
+
+    int startSimulation();
+
+private:
+    int width, height;
+    int w_width, w_height;
+    long double time;
+
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+
+    void update(double dt);
+    void draw();
+    int init();
+    void loop();
+    void handleEvents(SDL_Event &e, bool &running);
+    void kill();
+};
+#endif
