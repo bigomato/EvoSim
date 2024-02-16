5c5c0bcf66c9fb0386866ff18e43a912b217e7cf
Tim Büchner <buchnertim512@gmail.com>
2024-02-16 19:26:24 +0100

    added creature class

---
+++ src/creature.h
@@ -0,0 +1,36 @@
+#ifndef CREATURE_H
+#define CREATURE_H
+#include "utils/vec2.h"
+#include <SDL2/SDL.h>
+#include <map>
+
+using std::map;
+
+class Creature {
+public:
+  Creature(float age, float max_speed, float size, float full_health,
+           float current_health, float max_damage, float reproductice_drive,
+           float reproductivity, float distance_travelled);
+  ~Creature();
+
+  void move();
+  void attemptEat();
+  void attemptReproduce(Creature mate);
+  void attemptFight(Creature enemy);
+  void die();
+  map<int, float> sense();
+
+  void update();
+  void draw();
+
+private:
+  float age, max_speed, size, full_health, current_health, max_damage,
+      reproductice_drive, reproductivity, distance_travelled;
+  bool alive;
+  vec2<float> birth_position;
+  vec2<float> position;
+  // Brain brain
+  // DNA dna
+};
+
+#endif
