b4cbbec0697f0cb3546e875c39f930a920ff148a
Tim Büchner <buchnertim512@gmail.com>
2024-02-22 18:14:44 +0100

    Add ActionEmitter class and some derived classes

---
+++ src/ActionEmitter.cpp
@@ -0,0 +1,14 @@
+#include "ActionEmitter.h"
+#include "creature.h"
+#include <cmath>
+
+ActionEmitter::ActionEmitter(Node &node) : node (node) {};
+
+void MoveSpeedEmitter::emit(Creature *creature) {
+    creature->setSpeed(std::max(creature->max_speed, static_cast<float>(node.value))); // Cast node.value to float if necessary
+};
+
+void MoveDirectionEmitter::emit(Creature *creature) {
+    creature->setAngle(node.value);
+};
+
