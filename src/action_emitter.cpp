8654aa4a5fcbb123aa42345a9e6fceb314e0a5b0
NiniDev <linantfin@gmail.com>
2024-02-27 18:03:13 +0100

    Refactor ActionEmitter classes and fix include statements

--- src/action_emitter.cpp
+++ src/action_emitter.cpp
@@ -1,14 +1,23 @@
 #include "action_emitter.h"
 #include "creature.h"
-#include <cmath>
+#include <algorithm> // Include the necessary header file for std::max
 
-ActionEmitter::ActionEmitter(Node &node) : node (node) {};
+ActionEmitter::ActionEmitter()
+{
+    this->node = nullptr;
+}
 
-void MoveSpeedEmitter::emit(Creature *creature) {
-    creature->setSpeed(std::max(creature->max_speed, static_cast<float>(node.value))); // Cast node.value to float if necessary
+MoveSpeedEmitter::MoveSpeedEmitter(float speed) : ActionEmitter()
+{
+    this->speed = speed;
+}
+
+void MoveSpeedEmitter::emit(Creature *creature)
+{
+    creature->setSpeed(std::max(creature->max_speed, static_cast<float>(node->value)));
 };
 
-void MoveDirectionEmitter::emit(Creature *creature) {
-    creature->setAngle(node.value);
+void MoveDirectionEmitter::emit(Creature *creature)
+{
+    creature->setAngle(node->value);
 };
-
--- src/action_emitter.h
+++ src/action_emitter.h
@@ -4,31 +4,42 @@
 #include "world.h"
 #include "creature.h"
 
-class ActionEmitter {
-    public:
-        ActionEmitter(Node &node);
+class Creature;
 
-        virtual void emit(Creature *creature) = 0;
-    protected:
-        Node &node;
+class ActionEmitter
+{
+public:
+    ActionEmitter();
+
+    virtual void emit(Creature *creature) = 0;
+    void setNode(Node *node) { this->node = node; }
+    void setId(int id) { this->id = id; }
+
+protected:
+    Node *node;
+    int id;
 };
 
-class MoveSpeedEmitter : public ActionEmitter {
-    public:
-        MoveSpeedEmitter(Node &node, float speed);
+class MoveSpeedEmitter : public ActionEmitter
+{
+public:
+    MoveSpeedEmitter(float speed);
 
-        void emit(Creature *creature);
-    private:
-        float speed;
+    void emit(Creature *creature);
+
+private:
+    float speed;
 };
 
-class MoveDirectionEmitter : public ActionEmitter {
-    public:
-        MoveDirectionEmitter(Node &node, vec2<float> direction);
+class MoveDirectionEmitter : public ActionEmitter
+{
+public:
+    MoveDirectionEmitter(vec2<float> direction);
 
-        void emit(Creature *creature);
-    private:
-        vec2<float> direction;
+    void emit(Creature *creature);
+
+private:
+    vec2<float> direction;
 };
 
 #endif
