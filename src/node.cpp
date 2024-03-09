26124f3f0604836bfa4e5bf5ec58314e8a734a86
NiniDev <linantfin@gmail.com>
2024-03-09 13:52:03 +0100

    Node now has predefined activation functions

--- src/node.cpp
+++ src/node.cpp
@@ -1,9 +1,26 @@
 #include "node.h"
 
-Node::Node(int type, double bias, double (*lambda)(double x))
+Node::Node(int type, double bias, int activationFunction)
 {
     this->type = type;
     this->bias = bias;
     this->activationFunction = activationFunction;
     this->id = -1;
+}
+
+const std::map<int, std::function<double(double)>> Node::func_map = {
+    {0, [](double x)
+     { return x; }}, // Identity
+    {1, [](double x)
+     { return 1 / (1 + std::exp(-x)); }}, // Sigmoid
+    {2, [](double x)
+     { return std::tanh(x); }}, // Tanh
+    {3, [](double x)
+     { return std::max(0.0, x); }}, // ReLU
+    {4, [](double x)
+     { return (x > 0) ? 1 : 0; }}}; // Step
+
+double Node::activate(double x)
+{
+    return func_map.at(activationFunction)(x);
 }
--- src/node.h
+++ src/node.h
@@ -1,20 +1,27 @@
+#include <functional>
+#include <map>
+#include <algorithm>
+#include <cmath>
 #ifndef NODE_H
 #define NODE_H
 
 class Node
 {
 public:
-     Node(int type, double bias) : id(-1), type(type), value(0), bias(bias), activationFunction(nullptr){};
+     Node(int type, double bias) : id(-1), type(type), value(0), bias(bias), activationFunction(0){};
      Node(int id, int type, double value, double bias,
-          double (*activationFunction)(double));
-     Node(int type, double bias,
-          double (*lambda)(double x));
+          int activationFunction);
+     Node(int type, double bias, int activationFunction);
 
      int id;
      int type;
      double value;
      double bias;
-     double (*activationFunction)(double x);
+     int activationFunction;
+
+     static const std::map<int, std::function<double(double)>> func_map;
+
+     double activate(double x);
 };
 
 #endif
